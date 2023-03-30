273afccb54d4c245efd1e679ffe6299ab7bd1e04
Tan Le <tanlethanh8266@gmail.com>
2023-03-30 13:19:09 +0700

    Change ts config to js config

--- mobile-ui/babel.config.js
+++ mobile-ui/babel.config.js
@@ -10,7 +10,7 @@ module.exports = function (api) {
                 '@tamagui/babel-plugin',
                 {
                     components: ['tamagui'],
-                    config: './tamagui.config.ts',
+                    config: './tamagui.config.js',
                     logTimings: true
                 }
             ],
