6eb9d4093934cb79d9f718874dc2c2aff9f8432b
Tan Le <tanlethanh8266@gmail.com>
2023-04-02 21:42:48 +0700

    Wrap home screen by main layout

--- mobile-ui/screens/HomeScreen.jsx
+++ mobile-ui/screens/HomeScreen.jsx
@@ -1,4 +1,5 @@
 import { Button, ScrollView, Text, View } from 'react-native'
+import { MainLayout } from '../layouts/mainLayout'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { SheetDemo } from '../components/elements/BottomSheet'
 import { StatusBar } from 'expo-status-bar'
@@ -7,27 +8,23 @@ import BottomBar from '../components/BottomBar'
 
 function HomeScreen ({ navigation }) {
     return (
-        <SafeAreaView className="flex-1 bg-green-500">
-            <ScrollView>
-                <View className="flex items-center justify-center bg-slate-500">
-                    <Text className="text-white">Hello world</Text>
-                    <StatusBar style="auto" />
-                    <Button
-                        title="Go to Details"
-                        onPress={() => navigation.navigate('Details')}
-                    />
-                    <Button
-                        title="Go to Devices"
-                        onPress={() => navigation.navigate('Device')}
-                    />
-                    <SheetDemo></SheetDemo>
-                    <ToastDemo></ToastDemo>
-                    <Text>Hello world 2</Text>
-                </View>
-            </ScrollView>
-            <BottomBar></BottomBar>
-
-        </SafeAreaView>
+        <MainLayout>
+            <View className="flex items-center justify-center bg-slate-500">
+                <Text className="text-white">Hello world</Text>
+                <StatusBar style="auto" />
+                <Button
+                    title="Go to Details"
+                    onPress={() => navigation.navigate('Details')}
+                />
+                <Button
+                    title="Go to Devices"
+                    onPress={() => navigation.navigate('Device')}
+                />
+                <SheetDemo></SheetDemo>
+                <ToastDemo></ToastDemo>
+                <Text>Hello world 2</Text>
+            </View>
+        </MainLayout>
     )
 }
 
