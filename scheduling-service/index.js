cca9f1f25e267ccae6b7688008816db736d1c0ef
Tan Le <tanlethanh8266@gmail.com>
2023-04-02 22:39:21 +0700

    Add setup for mongoose and env

--- scheduling-service/index.js
+++ scheduling-service/index.js
@@ -1,15 +1,29 @@
 import express from 'express'
 import cors from 'cors'
+import * as dotenv from 'dotenv'
+import mongoose from 'mongoose'
 
 const app = express()
 
+dotenv.config()
+
+// Cors setup
 const corsOptions = {
     origin: '*',
     optionsSuccessStatus: 200
 }
-
 app.use(cors(corsOptions))
 
+// Database setup
+mongoose.set('strictQuery', false)
+
+mongoose.connect(process.env.DB_URI).then((conn) => {
+    return console.log('Database connected')
+}).catch((error) => {
+    console.log('Database connection fail', error)
+})
+
+// Lanch app
 const PORT = process.env.PORT || 8080
 app.listen(process.env.PORT || 8080, () =>
     console.log(`⚡️[server]: Server is running at https://localhost:${PORT}`)
