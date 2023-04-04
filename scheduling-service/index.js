edf8e7f72d1131e05555f23b78bd2b6144d6a0dc
Tan Le <tanlethanh8266@gmail.com>
2023-04-04 15:03:33 +0700

    Add basic api router

--- scheduling-service/index.js
+++ scheduling-service/index.js
@@ -2,6 +2,8 @@ import express from 'express'
 import cors from 'cors'
 import * as dotenv from 'dotenv'
 import mongoose from 'mongoose'
+import bodyParser from 'body-parser'
+import * as cronJob from './providers/cronJob.js'
 
 const app = express()
 
@@ -14,6 +16,8 @@ const corsOptions = {
 }
 app.use(cors(corsOptions))
 
+app.use(bodyParser.json())
+
 // Database setup
 mongoose.set('strictQuery', false)
 
@@ -23,8 +27,35 @@ mongoose.connect(process.env.DB_URI).then((conn) => {
     console.log('Database connection fail', error)
 })
 
+// API Route
+
+const schedulingApi = express.Router('/scheduling')
+
+schedulingApi
+    .get((req, res) => {
+        return res.json({
+            message: 'Hello world'
+        })
+    })
+    .post((req, res) => {
+        return res.json({
+            message: 'Hello world'
+        })
+    })
+    .delete('/:jobId', (req, res) => {
+        return res.json({
+            message: 'Hello world'
+        })
+    }).get('/:jobId', (req, res) => {
+        return res.json({
+            message: 'Hello world'
+        })
+    })
+
+app.use('/api/v1', schedulingApi)
+
 // Lanch app
 const PORT = process.env.PORT || 8080
 app.listen(process.env.PORT || 8080, () =>
-    console.log(`⚡️[server]: Server is running at https://localhost:${PORT}`)
+    console.log(`⚡️[server]: Server is running at http://localhost:${PORT}`)
 ).on('error', (error) => console.log('Error: ', error))
