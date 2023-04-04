ac54e06c24269186550757ede611a6469678bb68
Tan Le <tanlethanh8266@gmail.com>
2023-04-04 18:47:56 +0700

    Add scheduling option

--- scheduling-service/models/scheduling.js
+++ scheduling-service/models/scheduling.js
@@ -7,6 +7,11 @@ export const SchedulingStatus = {
     CANCEL: 'CANCEL'
 }
 
+export const SchedulingOption = {
+    EVERYDAY: 'EVERYDAY',
+    ONCE: 'ONCE'
+}
+
 const scheduling = new Schema({
     user_id: {
         type: String
@@ -26,7 +31,12 @@ const scheduling = new Schema({
     status: {
         type: String,
         enum: Object.values(SchedulingStatus),
-        default: 'INIT'
+        default: SchedulingStatus.INIT
+    },
+    option: {
+        type: String,
+        enum: Object.values(SchedulingOption),
+        default: SchedulingOption.ONCE
     }
 })
 
