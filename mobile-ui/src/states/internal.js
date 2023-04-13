5667f4f4b83560716a4ff0de651f6c1a45e0aab5
Tan Le <tanlethanh8266@gmail.com>
2023-04-13 17:04:01 +0700

    Add rooms data

---
+++ mobile-ui/src/states/internal.js
@@ -0,0 +1,14 @@
+export const rooms = [
+    {
+        key: "LR",
+        name: "Living Room",
+    },
+    {
+        key: "BR",
+        name: "Bedroom",
+    },
+    {
+        key: "SR",
+        name: "Study Room",
+    },
+];
