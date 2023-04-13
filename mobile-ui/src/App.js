d237bc1c934be8900fcab6c231c75da54ee70885
Tan Le <tanlethanh8266@gmail.com>
2023-04-13 18:39:47 +0700

    Waiting to loading device

--- mobile-ui/src/App.js
+++ mobile-ui/src/App.js
@@ -14,7 +14,7 @@ import { useColorScheme } from "react-native";
 
 import { FullLoading } from "./components/elements/FullLoading";
 import { initAllDevice, store } from "./states";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useFonts } from "expo-font";
 import config from "../tamagui.config";
 
@@ -25,16 +25,21 @@ Logs.enableExpoCliLogging();
 export default function App() {
     const colorScheme = useColorScheme();
 
+    const [deviceLoaded, setDeviceLoaded] = useState(false);
+
     const [loaded] = useFonts({
         Inter: require("@tamagui/font-inter/otf/Inter-Medium.otf"),
         InterBold: require("@tamagui/font-inter/otf/Inter-Bold.otf"),
     });
 
     useEffect(() => {
-        initAllDevice();
+        (async () => {
+            await initAllDevice();
+            setDeviceLoaded(true);
+        })();
     }, []);
 
-    return loaded ? (
+    return loaded && deviceLoaded ? (
         <TamaguiProvider config={config}>
             <Theme name={colorScheme === "dark" ? "dark" : "light"}>
                 <ToastProvider>
