e95ab966b057fbf8454ba0a2af447462e390d635
Tan Le <tanlethanh8266@gmail.com>
2023-04-13 20:32:17 +0700

    Refactor air conditioner screen

--- mobile-ui/src/screens/DeviceScreen/AirConditioner.jsx
+++ mobile-ui/src/screens/DeviceScreen/AirConditioner.jsx
@@ -1,32 +1,50 @@
 import { AirConditioner } from "../../components";
 import { DeviceLayout, MainLayout } from "../../layouts";
 // import { publishDeviceState, updateDeviceState } from "../../states";
+import { Text } from "react-native";
+import {
+    deviceTypes,
+    roomTypes,
+    selectDevices,
+    updateDeviceState,
+} from "../../states";
 import { useDispatch, useSelector } from "react-redux";
 
 export default function AirConditionerScreen({ route, navigation }) {
-    const { deviceId } = route.params | undefined;
-    const AirValue = deviceId
-        ? useSelector(
-              (state) =>
-                  state.devices.devicesList.find(
-                      (device) => Number(device.deviceId) == Number(deviceId),
-                  ).value,
-          )
-        : 0;
+    const { deviceKey } = route.params;
+    const AirDevice = useSelector(selectDevices)[deviceKey];
     const dispatch = useDispatch();
+
     const updateAirValue = (value) => {
-        if (deviceId != undefined) {
-            // dispatch(updateDeviceState(deviceId, value));
-            // dispatch(publishDeviceState(deviceId, value));
+        if (AirDevice != undefined) {
+            dispatch(
+                updateDeviceState({
+                    key: deviceKey,
+                    value,
+                }),
+            );
         }
     };
+
     return (
         <MainLayout>
-            <DeviceLayout deviceName="Air Device" navigation={navigation}>
-                <AirConditioner
-                    powerState={AirValue}
-                    callback={updateAirValue}
-                ></AirConditioner>
+            <DeviceLayout
+                roomName={
+                    roomTypes.find((ele) => ele.key === AirDevice.room).name
+                }
+                deviceName={
+                    deviceTypes.find((ele) => ele.key === AirDevice.type).name
+                }
+                navigation={navigation}
+            >
+                {AirDevice ? (
+                    <AirConditioner
+                        updateValue={updateAirValue}
+                        device={AirDevice}
+                    ></AirConditioner>
+                ) : (
+                    <Text>Not found this device</Text>
+                )}
             </DeviceLayout>
         </MainLayout>
     );
