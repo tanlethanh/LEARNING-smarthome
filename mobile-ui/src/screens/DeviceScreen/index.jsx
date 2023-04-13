f6d0d2e71104a97a6587ed7df2559e617afc27d2
Tan Le <tanlethanh8266@gmail.com>
2023-04-13 20:02:53 +0700

    Mount stack for all screen

--- mobile-ui/src/screens/DeviceScreen/index.jsx
+++ mobile-ui/src/screens/DeviceScreen/index.jsx
@@ -9,36 +9,28 @@ import LockScreen from "./Lock";
 const DeviceStack = createNativeStackNavigator();
 
 function DeviceScreen() {
-    const devices = useSelector(selectDevices);
-
-    const getScreen = (deviceType) => {
-        console.log(deviceType, "<-- type");
-        if (deviceType == "LIGHT") {
-            return LampScreen;
-        } else if (deviceType == "AIRCOND") {
-            return AirConditionerScreen;
-        } else if (deviceType == "LOCK") {
-            return LockScreen;
-        } else if (deviceType == "FAN") {
-            return FanScreen;
-        } else {
-            return FanScreen;
-        }
-    };
-
     return (
         <DeviceStack.Navigator>
-            {Object.values(devices).map((device, index) => {
-                console.log(device.id, "<--");
-                return (
-                    <DeviceStack.Screen
-                        key={index}
-                        name={String(device.id)}
-                        options={{ headerShown: false }}
-                        component={getScreen(device.type)}
-                    />
-                );
-            })}
+            <DeviceStack.Screen
+                name={"LIGHT"}
+                options={{ headerShown: false }}
+                component={LampScreen}
+            />
+            <DeviceStack.Screen
+                name={"AIRCOND"}
+                options={{ headerShown: false }}
+                component={AirConditionerScreen}
+            />
+            <DeviceStack.Screen
+                name={"LOCK"}
+                options={{ headerShown: false }}
+                component={LockScreen}
+            />
+            <DeviceStack.Screen
+                name={"FAN"}
+                options={{ headerShown: false }}
+                component={FanScreen}
+            />
         </DeviceStack.Navigator>
     );
 }
