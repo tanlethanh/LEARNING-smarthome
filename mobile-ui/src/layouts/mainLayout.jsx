a94a347b2eb5a2c93372b87f1d281bd3ae2b88b9
Tan Le <tanlethanh8266@gmail.com>
2023-04-13 17:02:59 +0700

    Add scroll flag into MainLayout

--- mobile-ui/src/layouts/mainLayout.jsx
+++ mobile-ui/src/layouts/mainLayout.jsx
@@ -2,11 +2,15 @@ import { SafeAreaView } from "react-native-safe-area-context";
 import { ScrollView } from "tamagui";
 import BottomBar from "../components/BottomBar";
 
-function MainLayout({ children }) {
+function MainLayout({ children, withScrollView = true }) {
     return (
         <SafeAreaView className="flex-1 bg-[#EFF1F5]">
+            {withScrollView ? (
+                <ScrollView className="min-h-screen">{children}</ScrollView>
+            ) : (
+                children
+            )}
             <BottomBar></BottomBar>
-            <ScrollView className="min-h-screen">{children}</ScrollView>
         </SafeAreaView>
     );
 }
