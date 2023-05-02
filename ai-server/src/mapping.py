73ef857e3062ea403413d25d2fbf5c933230b30b
Tan Le <tanlethanh8266@gmail.com>
2023-05-03 00:57:08 +0700

    Add simple mapping

---
+++ ai-server/src/mapping.py
@@ -0,0 +1,20 @@
+
+actions = {
+    "Turn_on": {
+        "value": 1
+    },
+    "Turn_off": {
+        "value": 1
+    }
+}
+
+room_map = {
+    "living_room": "LR",
+    "bed_room": "BR"
+}
+
+device_map = {
+    "light": "DEVICE_LIGHT",
+    "fan": "DEVICE_FAN",
+    "air_conditioner": "DEVICE_AIRCOND"
+}
