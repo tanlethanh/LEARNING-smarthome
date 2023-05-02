6fe1c2aae6d6fd0f38f0f6f73468d729d19c30db
Tan Le <tanlethanh8266@gmail.com>
2023-05-03 03:28:24 +0700

    Add handler for time part

--- ai-server/src/utils/command.test.py
+++ ai-server/src/utils/command.test.py
@@ -1,8 +1,9 @@
 from command import processing_text, parse_command
 import sys
 import json
-from pathlib import Path
 from adafruit import getAllFeeds, updateFeed
+from scheduling import addScheduling
+from datetime import datetime, timedelta, timezone
 
 f = open("../mapping.json")
 mapping = json.load(f)
@@ -13,28 +14,54 @@ def handle_command(cmd):
     if action is None:
         return "I don't understand the action, can you repeat again!"
     
+    action_str = " ".join(cmd.get("Ac").split("_"))
+    
     device = mapping["devices"].get(cmd.get("De"))
+    device_str = " ".join(cmd.get("De").split("_"))
+
     room = mapping["rooms"].get(cmd.get("Pos"))
+    room_str = " ".join(cmd.get("Pos").split("_"))
     name = "_".join([room, device])
 
     feeds = getAllFeeds()
     f_names = [f.name for f in feeds]
+    
+    time = cmd.get("Ti")
 
     if name in f_names:
         for f in feeds:
             if f.name == name:
                 key = f.key
                 value = action.get("value")
-                updateFeed(key, value)
-                return "Yes sir! The {} in {} is {}".format(" ".join(cmd.get("Pos").split("_")), " ".join(cmd.get("De").split("_")),  " ".join(cmd.get("Ac").split("_")))
+                
+                if time is not None:
+                    now = datetime.now(timezone.utc)
+                    if time['type'] == 'second':
+                        now += timedelta(seconds=time['num'])
+                    elif time['type'] == 'minute':
+                        now += timedelta(minutes=time['num'])
+                    elif time['type'] == 'hour':
+                        now += timedelta(hours=time['num'])
+                    
+                    try:
+                        addScheduling(f.key, value, now.isoformat())
+                        return "Alright! The {} in {} will be {} after {} {}".format(device_str, room_str, action_str, time['num'], time['type'])
+                    except Exception as e:
+                        print("Send scheduling request error ", e)
+                        return "Some thing go wrong, I can not set scheduling for you"
+                    
+                else:
+                    updateFeed(key, value)
+                    return "Yes sir! The {} in {} is {}".format(device_str, room_str,  action_str)
+                
         return "Some thing wrong"
         
     else:
         for fn in f_names:
             if room in fn:
-                return "Sorry! The {} doesn't have {} device".format(" ".join(cmd.get("Pos").split("_")), " ".join(cmd.get("De").split("_")))
-        return "Sorry! Are you sure that {} is a place in our hourse".format(" ".join(cmd.get("Pos").split("_")))
-    
+                return "Sorry! The {} doesn't have {} device".format(room_str, device_str)
+        return "Sorry! Are you sure that {} is a place in our hourse".format(room_str)
+        
 
 while True: 
     text = input("Type your command: ")
@@ -48,6 +75,7 @@ while True:
 
     if result is None:
         print("Res: I dont understand, can you repeat")
+        continue
 
     res = handle_command(result)
     print("Res: {}".format(res))
