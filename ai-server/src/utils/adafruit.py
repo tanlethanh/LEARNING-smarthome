745dc99699c6d604b8a078b7a6f1250bdc2db4d7
Tan Le <tanlethanh8266@gmail.com>
2023-05-03 01:46:36 +0700

    Add basic method of adafruit

--- ai-server/src/utils/adafruit.py
+++ ai-server/src/utils/adafruit.py
@@ -2,11 +2,11 @@ import os
 from dotenv import load_dotenv
 from pathlib import Path
 
-dotenv_path = Path('../../.env')
+dotenv_path = Path('../../../.env')
 load_dotenv(dotenv_path=dotenv_path)
 
 # Import library and create instance of REST client.
-from Adafruit_IO import Client
+from Adafruit_IO import Client, Data
 
 AIO_USERNAME = os.getenv('AIO_USERNAME')
 AIO_KEY = os.getenv('AIO_KEY')
@@ -14,9 +14,8 @@ print(AIO_USERNAME, AIO_KEY)
 
 aio = Client(AIO_USERNAME, AIO_KEY)
 
-# Get list of feeds.
-feeds = aio.feeds()
+def getAllFeeds():
+    return aio.feeds()
 
-# Print out the feed names:
-for f in feeds:
-    print(f)
+def updateFeed(feed_key, value):
+    return aio.create_data(feed_key, Data(value=value))
